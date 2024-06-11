209555fbf20afcbab4d436e9636f6c5eed7bf175
Douglas Melo <douglasmnegri@gmail.com>
2024-06-11 20:55:12 -0300

    add function to check entries, keys, values and clear hash map

--- hashMap.js
+++ hashMap.js
@@ -56,18 +56,73 @@ class HashMap {
 
     for (let i = 0; i < bucket.length; i++) {
       bucket.splice(i, 1);
+      this.growBuckets--;
       return true;
     }
   }
 
-//   print() {
-//     console.log(this.buckets);
-//   }
+  length() {
+    const bucket = this.buckets;
+    let count = 0;
+    for (let i = 0; i < bucket.length; i++) {
+      bucket[i] !== undefined ? count++ : (count += 0);
+    }
+    return count;
+  }
+
+  clear() {
+    const bucket = this.buckets;
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] !== undefined) {
+        for (let k = 0; k < bucket[i].length; k++) {
+          bucket[i].splice(k, 1);
+        }
+      }
+    }
+    this.growBuckets -= this.growBuckets;
+  }
+
+  keys() {
+    const arrayOfKeys = [];
+    const bucket = this.buckets;
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] !== undefined) {
+        const key = bucket[i].find((a) => {
+          arrayOfKeys.push(a.key);
+        });
+      }
+    }
+    return arrayOfKeys;
+  }
+
+  values() {
+    const arrayOfValues = [];
+    const bucket = this.buckets;
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] !== undefined) {
+        const value = bucket[i].find((a) => {
+          arrayOfValues.push(a.value);
+        });
+      }
+    }
+    return arrayOfValues;
+  }
+
+  entries() {
+    const arrayOfEntries = [];
+    const bucket = this.buckets;
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] !== undefined) {
+        const entries = bucket[i].find((a) => {
+          arrayOfEntries.push([a.key, a.value]);
+        });
+      }
+    }
+    return arrayOfEntries;
+  }
 }
 
 const hashMap = new HashMap();
-hashMap.set("Doug", "Melo");
-hashMap.set("Duda", "Pessoa");
-hashMap.set("Celia", "Baptista");
-console.log(hashMap.remove("Dex"));
-
